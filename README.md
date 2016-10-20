f86943aa6f97ecf6ce07ef2309f93d190e460e0c
Riley Tomasek <riley.tomasek@gmail.com>
2016-10-20 14:13:36 -0400

    ✨

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# eslint-changes
+# ESLint Changes
+
+ESLint changes is a script that is run by TravisCI for commits and pull
+requests. It runs ESLint on the files that were changed and adds the
+results to GitHub as a status.
+
+## Installation
+
+- Set GITHUB_TOKEN environment variable in Travis to be a GitHub access
+  token with repo privileges.
+- Install the eslint-changes package from npm during the Travis install
+  step:
+
+```
+install:
+  - npm install eslint-changes
+```
+
+- Run the ESLint Changes bin during the Travis script step:
+
+```
+script:
+  - ./node_modules/.bin/eslint-changes
+```
+
+## Details
+- ESLint Changes has `eslint: "3.x"` as a peer dependency. That means that
+  your project must already have eslint installed and configured.
+- ESLint Changes will use any configuration like .eslintrc, .eslintignore,
+  plugins or other configurations that are present in the project.
+- If you don't like installing eslint-changes from Travis, you can add it
+  to your projects package.json and add only the script part to
+  .travis.yml.
---
+++ index.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+
+const Github = require('github');
+const eslint = require('eslint');
+const exec = require('child-process-promise').exec;
+
+const cli = new eslint.CLIEngine();
+
+// Runs eslint on the array of file pathes and returns the lint results
+const runEslint = files => cli.executeOnFiles(cli.resolveFileGlobPatterns(files));
+
+// Get a promise of an array of the paths of *.js files that changed in the commit/PR
+const getChangedFilePaths = () => exec(`git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE} -- '*.js'`)
+  .then((result) => {
+    const files = result.stdout.split('\n');
+    // Remove the extra "" caused by the last newline
+    files.pop();
+    return files;
+  });
+
+// Gets the sha of the commit/pull request
+const getCommitTarget = (eventType) => {
+  let sha;
+  if (eventType === 'push') {
+    sha = process.env.TRAVIS_COMMIT;
+  } else if (eventType === 'pull_request') {
+    const travisCommitRange = process.env.TRAVIS_COMMIT_RANGE;
+    const parsed = travisCommitRange.split('...');
+    if (parsed.length === 1) {
+      sha = travisCommitRange;
+    } else {
+      sha = parsed[1];
+    }
+  } else {
+    // console.log(Colors.red('event type \'%s\' not supported'), eventType);
+    sha = null;
+  }
+
+  return sha;
+};
+
+// Add status with lint info to GitHub UI
+const setGithubStatus = (eslintReport) => {
+  const errors = eslintReport.errorCount;
+  const gh = new Github();
+  const parsedSlug = process.env.TRAVIS_REPO_SLUG.split('/');
+  const sha = getCommitTarget(process.env.TRAVIS_EVENT_TYPE);
+  const state = eslintReport.errorCount > 0 ? 'failure' : 'success';
+  const warnings = eslintReport.warningCount;
+
+  const description = `errors: ${errors} warnings: ${warnings}`;
+  const repo = parsedSlug[1];
+  const user = parsedSlug[0];
+  const targetUrl = `https://travis-ci.org/${process.env.TRAVIS_REPO_SLUG}/jobs/${process.env.TRAVIS_JOB_ID}`;
+
+  gh.authenticate({
+    token: process.env.GITHUB_TOKEN,
+    type: 'oauth',
+  });
+
+  gh.repos.createStatus({
+    context: 'ESLint Changes',
+    owner: user,
+    target_url: targetUrl,
+    description,
+    repo,
+    sha,
+    state,
+  });
+};
+
+const logEslintResults = (eslintReport) => {
+  const formatter = cli.getFormatter();
+  console.log(formatter(eslintReport.results));
+};
+
+getChangedFilePaths()
+  .then((files) => {
+    const eslintReport = runEslint(files);
+    setGithubStatus(eslintReport);
+    logEslintResults(eslintReport);
+  })
+  .catch(err => console.error('Error: ', err));
